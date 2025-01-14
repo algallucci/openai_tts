9161a41899648a0df75ef9600344e987e415af9a
algallucci <42651882+algallucci@users.noreply.github.com>
2025-01-14 17:26:17 -0500

    Update const.py

--- custom_components/openai_tts/const.py
+++ custom_components/openai_tts/const.py
@@ -5,6 +5,7 @@ CONF_API_KEY = 'api_key'
 CONF_MODEL = 'model'
 CONF_VOICE = 'voice'
 CONF_SPEED = 'speed'
+CONF_URL = 'url'
 MODELS = ["tts-1", "tts-1-hd", "kokoro"]
 VOICES = ["alloy", "echo", "fable", "onyx", "nova", "shimmer", "batman","af_sky+af_bella", "af_sky", "af_bella"]
 URL = ["http://192.168.68.52:8000/v1/audio/speech", "http://192.168.68.52:8880/v1/audio/speech"]
